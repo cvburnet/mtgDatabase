46af788344810196db9c3e33174a833f814c9beb
C.V. Burnet <cvburnet@uabmc.edu>
2025-06-16 11:14:04 -0500

    include documentation

--- R/import.R
+++ R/import.R
@@ -1,2 +1,7 @@
+# Data can be found at https://mtgjson.com/api/v5/csv/.
+
+# Documentation on the nature of the data can be found at:
+  # https://mtgjson.com/data-models/card/card-atomic/
+
 all_cards <- read.csv(file = "./input/cards.csv")
 save(all_cards, file = "./output/all_cards.Rda")
