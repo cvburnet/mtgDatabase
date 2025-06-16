149560836c5c1464c0a3ce2193a41fc23c697ce6
C.V. Burnet <cvburnet@uabmc.edu>
2025-06-16 11:14:21 -0500

    fix printings, include more vars

--- R/trim.R
+++ R/trim.R
@@ -3,9 +3,10 @@ load("./output/all_cards.Rda")
 vars_needed <- c("uuid","name","manaCost","manaValue",
                  "type","supertypes","types","subtypes","side","colors",
                  "power","toughness","loyalty","defense","text","keywords",
-                 "printings")
+                 "printings","hasFoil","hasNonFoil","flavorText")
 
 my_cards <- all_cards[,vars_needed]
+my_cards$printings <- str_split(all_cards[,"printings"],", ")
 
 legend_index <- (my_cards$supertypes == "Legendary")
 legend_cards <- my_cards[legend_index,]
