4e47b288998fab6c0776e81d84ebbd995a58dc65
C. V. Burnet <cvburn@gmail.com>
2025-06-30 13:52:47 -0500

    edit: include functionality for trim.R

--- R/trim.R
+++ R/trim.R
@@ -1,12 +1,27 @@
 load("./output/all_cards.Rda")
 
+# Inclusion Criteria: 
+# 1.) Must be printed on paper, and not oversized. (isOnlineOnly, isOversized)
+# 2.) Must not have been from a "funny" set. (isFunny)
+# 3.) 
+
+trimmed_cards <- all_cards |>
+  filter(isOnlineOnly == "" & isOversized == "") |>
+  filter(isFunny == "")
+
 vars_needed <- c("uuid","name","manaCost","manaValue",
                  "type","supertypes","types","subtypes","side","colors",
                  "power","toughness","loyalty","defense","text","keywords",
-                 "printings","hasFoil","hasNonFoil","flavorText")
+                 "printings","setCode","isReprint","hasFoil","hasNonFoil","flavorText",
+                 "isAlternative","isFullArt")
 
-my_cards <- all_cards[,vars_needed]
-my_cards$printings <- str_split(all_cards[,"printings"],", ")
+my_cards <- trimmed_cards[,vars_needed]
+my_cards$printings <- str_split(trimmed_cards[,"printings"],", ")
+
+unique_cards <- my_cards[which(!duplicated(my_cards$name)),]
+unique_cards2 <- my_cards |>
+  filter(isReprint == "" & isAlternative == "" & isFullArt == "")
+
 
 legend_index <- (my_cards$supertypes == "Legendary")
 legend_cards <- my_cards[legend_index,]
